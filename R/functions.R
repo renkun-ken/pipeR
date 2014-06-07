06e9f73a7a2a485581fe38035d97ecdf5c92fba6
Kun Ren <renkun@outlook.com>
2014-06-07 22:32:08 +0800

    performance improvement

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,5 @@
 .pipe <- function(.,fun) {
-  fun <- as.list(substitute(fun))
+  fun <- as.vector(substitute(fun),"list")
   eval(as.call(c(fun[1],substitute(.),fun[-1])),
     envir = parent.frame(),enclos = NULL)
 }
