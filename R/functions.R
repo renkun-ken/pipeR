3342a7acbb72d36642332dcce0c122e1487df55f
Kun Ren <renkun@outlook.com>
2014-06-07 21:56:27 +0800

    NULL enclosing env

--- R/functions.R
+++ R/functions.R
@@ -1,17 +1,17 @@
 .pipe <- function(.,fun) {
   fun <- as.list(substitute(fun))
   eval(as.call(c(fun[1],substitute(.),fun[-1])),
-    envir = parent.frame(),enclos = baseenv())
+    envir = parent.frame(),enclos = NULL)
 }
 
 .fpipe <- function(.,expr) {
   env <- new.env(hash = FALSE,parent = parent.frame(),size = 1)
   assign(".",.,envir = env,inherits = FALSE)
-  eval(substitute(expr),envir = env,enclos = baseenv())
+  eval(substitute(expr),envir = env,enclos = NULL)
 }
 
 .lpipe <- function(.,lambda) {
   env <- new.env(hash = FALSE,parent = parent.frame(),size = 1)
   assign(as.character(lambda[[2]]),.,envir = env,inherits = FALSE)
-  eval(lambda[[3]],envir = env,enclos = baseenv())
+  eval(lambda[[3]],envir = env,enclos = NULL)
 }
