b14e1fc2a6bee77450801aee076ad0db1174b587
Kun Ren <renkun@outlook.com>
2014-07-26 13:21:35 +0800

    refine

--- R/functions.R
+++ R/functions.R
@@ -1,17 +1,18 @@
+setnames <- `names<-`
+setclass <- `class<-`
+
 .pipe <- function(.,fun) {
-  fun <- as.vector(substitute(fun),"list")
+  fun <- setclass(substitute(fun),"list")
   eval(as.call(c(fun[1L],substitute(.),fun[-1L])),
-    envir = parent.frame(),enclos = NULL)
+    envir=parent.frame(),enclos = NULL)
 }
 
 .fpipe <- function(.,expr) {
-  env <- new.env(FALSE,parent.frame(),1L)
-  env$. <- .
-  eval(substitute(expr),env,NULL)
+  eval(substitute(expr),list(.=.),parent.frame())
 }
 
 .lpipe <- function(.,lambda) {
-  env <- new.env(FALSE,parent.frame(),1L)
-  assign(as.character(lambda[[2L]]),.,envir = env)
-  eval(lambda[[3L]],env,NULL)
+  eval(lambda[[3L]],
+    setnames(list(.),as.character.default(lambda[[2L]])),
+    parent.frame())
 }
