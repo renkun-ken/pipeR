5973ce055d007d7619814bdca2a31fe3a93e7475
Kun Ren <renkun@outlook.com>
2014-08-04 20:27:52 +0800

    major api change

--- R/functions.R
+++ R/functions.R
@@ -1,18 +1,45 @@
 setnames <- `names<-`
 setclass <- `class<-`
 
-.pipe <- function(x,fun) {
-  fun <- setclass(substitute(fun),"list")
-  eval(as.call(c(fun[1L],quote(x),fun[-1L])),
-    envir=list(x=x),enclos = parent.frame())
+pipe.first <- function(x,fun,envir) {
+  fun <- setclass(fun,"list")
+  eval(as.call(c(fun[1L],quote(.),fun[-1L])),
+    envir=list(.=x),enclos = envir)
 }
 
-.fpipe <- function(.,expr) {
-  eval(substitute(expr),list(.=.),parent.frame())
+pipe.dot <- function(.,expr,envir) {
+  eval(expr,list(.=.),envir)
 }
 
-.lpipe <- function(x,lambda) {
-  eval(lambda[[3L]],
-    setnames(list(x),as.character.default(lambda[[2L]])),
-    parent.frame())
+eval.labmda <- function(x,symbol,expr,envir) {
+  eval(expr,setnames(list(x),as.character.default(symbol)),envir)
+}
+
+pipe.lambda <- function(x,expr,envir) {
+  if(is.call(expr)) {
+    symbol <- as.character.default(expr[[1L]])
+    if(length(symbol) == 1L) {
+      if(symbol == "~") {
+        return(eval.labmda(x,expr[[2L]],expr[[3L]],envir))
+      } else if(symbol == "<-") {
+        return(eval.labmda(x,expr[[3L]],expr[[2L]],envir))
+      }
+    }
+  }
+  eval(expr,list(.=x),envir)
+}
+
+pipe.op <- function(x,expr) {
+  expr <- substitute(expr)
+  if(is.call(expr)) {
+    symbol <- as.character.default(expr[[1L]])
+    if(length(symbol) == 1L) {
+      if(symbol == "{") {
+        return(pipe.dot(x,expr,parent.frame()))
+      } else if(symbol == "(") {
+        return(pipe.lambda(x,expr[[2L]],parent.frame()))
+      }
+    }
+  }
+  pipe.first(x,expr,parent.frame())
 }
--- R/pipeR.R
+++ R/pipeR.R
@@ -1,66 +1,68 @@
-#' Pipe an object forward as the first argument to a function call
+#' Pipe an object forward
 #'
-#' The \code{\%>>\%} operator evaluates the function call on the right-hand side
-#' with the left-hand side object being the first argument.
+#' The \code{\%>>\%} operator pipes the object on the left-hand side to the
+#' right-hand side as either the first argument or a symbol defined by lambda
+#' expression.
 #'
-#' @param x The object to pipe as the first argument
-#' @param fun The function call to evaluate with the piped object as the first argument.
-#' @name first-argument piping
+#' @param x object
+#' @param expr expression
 #' @export
 #' @examples
 #' \dontrun{
+#' # Pipe as first-argument to a function name
 #' rnorm(100) %>>% plot
+#'
+#' # Pipe as first-argument to a function call
+#' rnorm(100) %>>% plot()
 #' rnorm(100) %>>% plot(col="red")
-#' rnorm(1000) %>>% sample(size=100,replace=F) %>>% hist
-#' # pipe to a function in namespace
-#' 1:10 %>>% base::mean()
-#' # note that "()" must follow the function in namespace.
+#' rnorm(100) %>>% plot(col="red",main=length(.))
+#'
+#' # Pipe as first-argument to a function call in namespace
+#' rnorm(100) %>>% base::mean()
+#'
+#' # Pipe to an expression enclosed by braces with .
+#' representing the piped object
+#' rnorm(100) %>>% { plot(.,col="red",main=length(.)) }
+#'
+#' # Pipe to an expression enclosed by parentheses with .
+#' representing the piped object
+#' rnorm(100) %>>% (plot(.,col="red",main=length(.)))
+#'
+#' # Pipe to an expression enclosed by parentheses with
+#' lambda expression in the form of x -> f(x) or x ~ f(x).
+#' rnorm(100) %>>% (x -> plot(x,col="red",main=length(x)))
+#' rnorm(100) %>>% (x ~ plot(x,col="red",main=length(x)))
+#'
+#' # Pipe to fun to use lambda expression
+#' rnorm(100) %>>% fun(. + 1)
+#' rnorm(100) %>>% fun(x -> x + 1)
+#' rnorm(100) %>>% fun(x ~ x + 1)
+#'
+#' # Pipe to an anomymous function
+#' rnorm(100) %>>% (function(x) mean(x))()
+#' rnorm(100) %>>% {function(x) mean(x)}()
+#'
+#' # Pipe to an enclosed function to make a closure
+#' z <- rnorm(100) %>>% (function(x) mean(x+.))
+#' z(1) # 3
+#'
+#' z <- rnorm(100) %>>% {function(x) mean(x+.)}
+#' z(1) # 3
+#'
+#' # Data manipulation with dplyr
+#' library(dplyr)
+#' iris %>>%
+#'   mutate(Sepal.Size=Sepal.Length*Sepal.Width,
+#'     Petal.Size=Petal.Length*Petal.Width) %>>%
+#'   select(Sepal.Size,Petal.Size,Species) %>>%
+#'   group_by(Species) %>>%
+#'   do(arrange(.,desc(Sepal.Size+Petal.Size)) %>>% head(3))
+#'
+#' # Data manipulation with rlist
+#' library(rlist)
+#' list(1,2,3) %>>%
+#'   list.map(. + 1) %>>%
+#'   list.filter(. <= 5) %>>%
+#'   list.sort(.)
 #' }
-`%>>%` <- .pipe
-
-#' Pipe an object forward as \code{.} to an expression
-#'
-#' The operator \code{\%:>\%} evaluates the expression on the right-hand side
-#' with the left-hand side object referred to as \code{.}.
-#'
-#' @param . The object to be piped as represented by \code{.}
-#' @param expr The expression to evaluate with the piped object referred to as \code{.}
-#' @name free-piping
-#' @export
-#' @examples
-#' \dontrun{
-#' rnorm(100) %:>% plot(.)
-#'
-#' rnorm(100) %:>% plot(.,col="red")
-#'
-#' rnorm(1000) %:>% sample(.,size=length(.)*0.1,replace=FALSE)
-#'
-#' rnorm(1000) %:>%
-#'   sample(.,length(.)*0.1,FALSE) %>>%
-#'   plot(.,main=sprintf("length: %d",length(.)))
-#' }
-`%:>%` <- .fpipe
-
-#' Pipe an object by lambda expression
-#'
-#' The operator \code{\%|>\%} pipes the left-hand side to the
-#' symbol defined by the lambda expression on the right-hand side
-#' and evaluates the target expression.
-#'
-#' @param x The object to be piped
-#' @param lambda The lambda expression which should always be in the form like \(x ~ g\(x\)\)
-#' @name lambda-piping
-#' @export
-#' @examples
-#' \dontrun{
-#' rnorm(100) %|>% (x ~ plot(x))
-#'
-#' rnorm(100) %|>% (x ~ plot(x,col="red"))
-#'
-#' rnorm(1000) %|>% (pop ~ sample(pop,length(pop)*0.2,FALSE))
-#'
-#' rnorm(1000) %|>%
-#'   (pop ~ sample(pop,length(pop)*0.2,FALSE)) %|>%
-#'   (s ~ plot(s,main=sprintf("length: sample: %d",length(s))))
-#' }
-`%|>%` <- .lpipe
+`%>>%` <- pipe.op
