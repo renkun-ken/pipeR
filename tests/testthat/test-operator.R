d9d45e133c09211564b9fe34a81b6da8fe9b3e72
Kun Ren <renkun@outlook.com>
2014-08-04 20:28:42 +0800

    refine test cases for operator

--- tests/testthat/test-operator.R
+++ tests/testthat/test-operator.R
@@ -1,6 +1,6 @@
-context("First-argument piping")
+context("Operator tests")
 
-test_that("First-argument piping works as expected", {
+test_that("first-argument piping", {
 
   # ordinary usages
   expect_identical(1:10 %>>% sin %>>% sum, sum(sin(1:10)))
@@ -8,6 +8,9 @@ test_that("First-argument piping works as expected", {
   expect_identical(iris %>>% names, names(iris))
   expect_identical(iris %>>% head(n=3),head(iris,n=3))
   expect_identical("a" %>>% switch(a=1,b=2,c=3),switch("a",a=1,b=2,c=3))
+
+  expect_identical(1:3 %>>% base::mean(), mean(1:3))
+  expect_identical(1:3 %>>% c(1,2,.), c(1:3,1,2,1:3))
 
   # working with higher-order functions
   expect_identical(1:5 %>>% lapply(function(i) i+1), lapply(1:5,function(i) i+1))
@@ -24,4 +27,23 @@ test_that("First-argument piping works as expected", {
   }
 
   expect_identical(1:10 %>>% fun2(a=-1,b=1), fun1(1:10,a=-1,b=1))
+})
+
+test_that("lambda piping", {
+  expect_identical(1:3 %>>% (c(1,2,.)), c(1,2,1:3))
+  expect_identical(1:3 %>>% {c(1,2,.)}, c(1,2,1:3))
+  expect_identical(1:3 %>>% (x -> c(1,2,x)), c(1,2,1:3))
+  expect_identical(1:3 %>>% (x ~ c(1,2,x)), c(1,2,1:3))
+})
+
+test_that("function", {
+  # anonymous function
+  expect_identical(1:3 %>>% (function(x) mean(x))(), 2)
+  expect_identical(1:3 %>>% (function(x,y) mean(x+y))(3), 5)
+
+  # closure
+  expect_identical({
+    z <- 1:3 %>>% (function(x) mean(x+.))
+    z(3)
+  }, 5)
 })
