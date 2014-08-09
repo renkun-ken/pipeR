e390f01018f73f3792f9e326ec5377ead31197d0
Kun Ren <ken@renkun.me>
2014-08-09 22:34:58 +0800

    add comments

--- R/functions.R
+++ R/functions.R
@@ -1,20 +1,37 @@
 setnames <- `names<-`
 setclass <- `class<-`
 
+# pipe to first argument
+# x : object
+# fun : the function name or call
+# envir : environment for evaluation
 pipe.first <- function(x,fun,envir) {
   fun <- setclass(fun,"list")
   eval(as.call(c(fun[1L],quote(.),fun[-1L])),
     envir=list(.=x),enclos = envir)
 }
 
+# pipe to dot
+# . : object
+# expr : expression
+# envir : environment for evaluation
 pipe.dot <- function(.,expr,envir) {
   eval(expr,list(.=.),envir)
 }
 
+# evaluate lambda expression
+# x : object
+# symbol : symbol part
+# expr : expression part
+# envir : environment for evaluation
 eval.labmda <- function(x,symbol,expr,envir) {
   eval(expr,setnames(list(x),as.character(symbol)),envir)
 }
 
+# pipe by lambda expression
+# x : object
+# expr : lambda expression
+# envir : environment for evaluation
 pipe.lambda <- function(x,expr,envir) {
   if(is.call(expr)) {
     symbol <- as.character(expr[[1L]])
@@ -28,6 +45,9 @@ pipe.lambda <- function(x,expr,envir) {
   eval(expr,list(.=x),envir)
 }
 
+# pipe function that determines the piping mechanism for the expression
+# x : object
+# expr : function name, call, or enclosed expression
 pipe.op <- function(x,expr) {
   expr <- substitute(expr)
   if(is.call(expr)) {
